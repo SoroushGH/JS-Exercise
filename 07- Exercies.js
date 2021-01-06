49487a2a36563fffdb3356735c733c4722a17799
soroush <s.ghodrat@gmail.com>
2021-01-06 18:09:47 +0100

    commit

--- 07- Exercies.js
+++ 07- Exercies.js
@@ -1,3 +1,14 @@
-var arr = ["banana", "apple", "orange"];
-var arr2 = ["tv", "dvd-player", "playstation"];
-var pos = 2;
+var arr = ["banana", "apple", "orange"]
+var arr2 = ["tv", "dvd-player", "playstation"]
+var pos = 2
+var resultArray = []
+
+function swap(arr, arr2, pos) {
+    let temp = arr[pos]
+    arr[pos] = arr2[pos]
+    arr2[pos] = temp
+    return resultArray = [arr, arr2]
+
+}
+
+swap(arr, arr2, pos)
