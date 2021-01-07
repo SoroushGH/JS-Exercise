f811eb7b608f2fdc9eba8fa75f88c66b63a1e48b
soroush <s.ghodrat@gmail.com>
2021-01-07 16:03:00 +0100

    commit

---
+++ 14- Exercies.js
@@ -0,0 +1,7 @@
+var arr = ["glass", "car", "watch", "sofa", "macbook"];
+
+function findPosition(arr, element) {
+    return arr.indexOf(element)
+}
+
+findPosition(arr, "car")
---
+++ 15- Exercies.js
@@ -0,0 +1,7 @@
+var arr = ["green", "red", "black", "blue", "brown", "yellow", "purple"]
+
+function isThere(arr, element){
+    return arr.includes(element)
+}
+
+isThere(arr, "red")
