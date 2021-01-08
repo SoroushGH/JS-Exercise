d6e33fdf1e9500306948edd117733ffd91a037b2
soroush <s.ghodrat@gmail.com>
2021-01-08 11:50:04 +0100

    commit

---
+++ 21- Exercise.js
@@ -0,0 +1,14 @@
+function last(array, n){
+    if(n === undefined){
+        return array.splice(-1)
+    }
+    else{
+        array.
+    }
+}
+
+
+
+console.log(last([7, 9, 0, -2]));
+console.log(last([7, 9, 0, -2], 3));
+console.log(last([7, 9, 0, -2], 6));
