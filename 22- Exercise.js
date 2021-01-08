e4a3489c1c62eaa2f0a76fc8bde30e9ce1fb221a
soroush <s.ghodrat@gmail.com>
2021-01-08 15:12:28 +0100

    commit

---
+++ 22- Exercise.js
@@ -0,0 +1,9 @@
+myColor = ["Red", "Green", "White", "Black"];
+
+function join(array) {
+    console.log(array.toString())
+    console.log(array.join(','))
+    console.log(array.join('+'))
+}
+
+console.log(join(myColor))
