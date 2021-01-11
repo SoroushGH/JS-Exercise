0c192b088c26e3dde35378f4c2ded860131e2074
soroush <s.ghodrat@gmail.com>
2021-01-11 15:32:49 +0100

    commit

---
+++ 27- Exercise.js
@@ -0,0 +1,20 @@
+let arr1 = ["12", "55", "hello"]
+let arr2 = ["12", "55", true, false, true, true, true]
+let arr3 = ["12", "55", true, false, true, true, true]
+
+
+function booleanChecker(arr, maxCapacity) {
+    let result = [];
+    arr.forEach((item) => {
+        if (typeof item === "boolean" && result.length < maxCapacity) {
+            result.push(item);
+        } else if (maxCapacity === undefined && typeof item === "boolean") {
+            maxCapacity = arr.length;
+            result.push(item);
+        }
+        //console.log(result);
+    });
+    return `${result.length} booleans were found ${result}`;
+}
+
+booleanChecker(arr, 3);
