dea9de6e705b91a024d9683b259f2b0d92cae5b1
soroush <s.ghodrat@gmail.com>
2021-01-11 09:31:02 +0100

    commit

---
+++ 24- Exercise.js
@@ -0,0 +1,21 @@
+var arr1 = [3, 8, 7, 6, 13, 5, -4, 3, 2, 66, 1]
+
+function sortArray(input) {
+    return input.sort()
+}
+
+// function sortArray(input) {
+//     output2 = arr1.sort((a, b) => {
+//         return ("output2: ", a - b)
+//     })
+//     return output2
+// }
+
+// function sortArray(input) {
+//     output3 = arr1.sort((a, b) => {
+//         return ("output3: ", b - a)
+//     })
+//     return output3
+// }
+
+sortArray(arr1)
