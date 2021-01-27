9c15ab76b2a918751a0858603b169405b44c8f00
soroush <s.ghodrat@gmail.com>
2021-01-27 09:13:09 +0100

    commit

--- 37- Exercise.js
+++ 37- Exercise.js
@@ -1,11 +1,28 @@
-function fibonacci(num) {
+// Solution #1
+function fibonacci1(num) {
     var fibo = [0, 1];
 
     for (var i = 0; i <= num; i++) {
         fibo.push(fibo[i] + fibo[i + 1])
     }
     console.log(fibo)
-    return `${fibo[num]}`
+    return fibo[num]
 }
 
-fibonacci(9);
+fibonacci1(9);
+
+
+// Solution #2
+
+function fibonacci2(num) {
+    const result = [0, 1]
+
+    for (let i = 2; i <= num; i++) {
+        const prevNum1 = result[i - 1]
+        const prevNum2 = result[i - 2]
+        result.push(prevNum1 + prevNum2)
+    }
+    return result[num]
+}
+
+fibonacci2(5)
