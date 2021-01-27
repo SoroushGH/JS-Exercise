4d0cac9d78b2b8873e82a08a50c233f9bb383e06
soroush <s.ghodrat@gmail.com>
2021-01-27 10:46:51 +0100

    commit

--- 39- Exercise.js
+++ 39- Exercise.js
@@ -1,15 +1,15 @@
 function fizzBuzz(item) {
     for (let i = 1; i <= item; i++) {
-        if (i % 5 === 0 && item % 3 === 0) {
-            console.log('Fizz')
-        } else if (i % 3 === 0) {
-            console.log('Buzz')
-        } else if (i % 5 === 0) {
+        if (i % 5 === 0 && i % 3 === 0) {
             console.log('Fizz Buzz')
+        } else if (i % 3 === 0) {
+            console.log('Fizz')
+        } else if (i % 5 === 0) {
+            console.log('Buzz')
         } else {
             console.log(i)
         }
     }
 }
 
-fizzBuzz(5)
+fizzBuzz(15)
